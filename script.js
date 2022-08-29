8c64fff3a7f978d3b4efce51c6accad982a6ce95
KingTayte <73963278+KingTayte@users.noreply.github.com>
2022-08-29 23:46:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunc(){
+    if(document.getElementById("myusername") == "tatenda.charlesm@gmail.com" && document.getElementById("mypassword") == "Tatendac#10"){
+        alert("Test Successful")
+    }else{
+        alert("Error");
+    }
+}
