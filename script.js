b2ea7cd8abce48e2619a740df045faff72817edd
KingTayte <73963278+KingTayte@users.noreply.github.com>
2022-08-30 17:15:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function myFunc(){
-    if(document.getElementById("myusername") == "tatenda.charlesm@gmail.com" && document.getElementById("mypassword") == "Tatendac#10"){
-        alert("Test Successful")
-    }else{
-        alert("Error");
-    }
-}
